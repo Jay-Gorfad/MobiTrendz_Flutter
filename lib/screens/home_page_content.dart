20e4baae8fdccef268d9dfbd38f35e1d87c0dd20
Jay <jgorfad223@rku.ac.in>
2025-05-15 21:07:41 +0530

    search and filter completed

--- lib/screens/home_page_content.dart
+++ lib/screens/home_page_content.dart
@@ -4,6 +4,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:mobitrendz/constants/app_constants.dart';
 import 'package:mobitrendz/screens/product_details.dart';
+import 'package:mobitrendz/screens/search_screen.dart';
 import 'category_screen.dart';
 import '../controllers/category_controller.dart';
 import '../models/category_model.dart';
@@ -105,18 +106,25 @@ class _HomePageContentState extends State<HomePageContent> {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             // Profile
-            Row(
-              children: [
-                CircleAvatar(
-                  radius: 26,
-                  backgroundImage: AssetImage("assets/profiles.png"),
+            RichText(
+              text: const TextSpan(
+                style: TextStyle(
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 1.2,
+                  fontFamily: 'Montserrat', // or any stylish custom font
                 ),
-                const SizedBox(width: 12),
-                const Text(
-                  "Jay Gorfad",
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
-                ),
-              ],
+                children: [
+                  TextSpan(
+                    text: 'Mobi',
+                    style: TextStyle(color: Colors.black87),
+                  ),
+                  TextSpan(
+                    text: 'Trendz',
+                    style: TextStyle(color: Colors.deepPurple),
+                  ),
+                ],
+              ),
             ),
             const SizedBox(height: 20),
 
@@ -139,6 +147,7 @@ class _HomePageContentState extends State<HomePageContent> {
                   border: InputBorder.none,
                   contentPadding: const EdgeInsets.symmetric(vertical: 14),
                 ),
+                onTap: () => Get.to(() => SearchView()),
               ),
             ),
             const SizedBox(height: 18),
@@ -200,75 +209,75 @@ class _HomePageContentState extends State<HomePageContent> {
             }),
             const SizedBox(height: 18),
             const SizedBox(height: 20),
-            const Text("Promo Codes",
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-            const SizedBox(height: 12),
-            Obx(() {
-              if (isPromoLoading.value) {
-                return Center(child: CircularProgressIndicator());
-              }
-              if (promoCodes.isEmpty) {
-                return Center(child: Text("No Promo Codes Available"));
-              }
-              return SizedBox(
-                height: 110,
-                child: ListView.builder(
-                  scrollDirection: Axis.horizontal,
-                  itemCount: promoCodes.length,
-                  itemBuilder: (context, index) {
-                    final promo = promoCodes[index];
-                    return Container(
-                      width: 220,
-                      margin: const EdgeInsets.only(right: 12),
-                      padding: const EdgeInsets.all(12),
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(12),
-                        border: Border.all(color: Colors.grey.shade300),
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.black12,
-                            blurRadius: 4,
-                            offset: Offset(0, 2),
-                          )
-                        ],
-                      ),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            promo["name"] ?? "",
-                            style: TextStyle(
-                                fontSize: 16,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.black87),
-                          ),
-                          const SizedBox(height: 4),
-                          Text(
-                            promo["description"] ?? "",
-                            maxLines: 2,
-                            overflow: TextOverflow.ellipsis,
-                            style: TextStyle(
-                                fontSize: 13, color: Colors.grey[600]),
-                          ),
-                          const SizedBox(height: 4),
-                          if (promo["discount"] != null &&
-                              promo["discount"] > 0)
-                            Text(
-                              "${promo["discount"]}% OFF",
-                              style: TextStyle(
-                                fontSize: 13,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.green,
-                              ),
-                            ),
-                        ],
-                      ),
-                    );
-                  },
-                ),
-              );
-            }),
+            // const Text("Promo Codes",
+            //     style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+            // const SizedBox(height: 12),
+            // Obx(() {
+            //   if (isPromoLoading.value) {
+            //     return Center(child: CircularProgressIndicator());
+            //   }
+            //   if (promoCodes.isEmpty) {
+            //     return Center(child: Text("No Promo Codes Available"));
+            //   }
+            //   return SizedBox(
+            //     height: 110,
+            //     child: ListView.builder(
+            //       scrollDirection: Axis.horizontal,
+            //       itemCount: promoCodes.length,
+            //       itemBuilder: (context, index) {
+            //         final promo = promoCodes[index];
+            //         return Container(
+            //           width: 220,
+            //           margin: const EdgeInsets.only(right: 12),
+            //           padding: const EdgeInsets.all(12),
+            //           decoration: BoxDecoration(
+            //             color: Colors.white,
+            //             borderRadius: BorderRadius.circular(12),
+            //             border: Border.all(color: Colors.grey.shade300),
+            //             boxShadow: [
+            //               BoxShadow(
+            //                 color: Colors.black12,
+            //                 blurRadius: 4,
+            //                 offset: Offset(0, 2),
+            //               )
+            //             ],
+            //           ),
+            //           child: Column(
+            //             crossAxisAlignment: CrossAxisAlignment.start,
+            //             children: [
+            //               Text(
+            //                 promo["name"] ?? "",
+            //                 style: TextStyle(
+            //                     fontSize: 16,
+            //                     fontWeight: FontWeight.bold,
+            //                     color: Colors.black87),
+            //               ),
+            //               const SizedBox(height: 4),
+            //               Text(
+            //                 promo["description"] ?? "",
+            //                 maxLines: 2,
+            //                 overflow: TextOverflow.ellipsis,
+            //                 style: TextStyle(
+            //                     fontSize: 13, color: Colors.grey[600]),
+            //               ),
+            //               const SizedBox(height: 4),
+            //               if (promo["discount"] != null &&
+            //                   promo["discount"] > 0)
+            //                 Text(
+            //                   "${promo["discount"]}% OFF",
+            //                   style: TextStyle(
+            //                     fontSize: 13,
+            //                     fontWeight: FontWeight.bold,
+            //                     color: Colors.green,
+            //                   ),
+            //                 ),
+            //             ],
+            //           ),
+            //         );
+            //       },
+            //     ),
+            //   );
+            // }),
 
             const SizedBox(height: 20),
             const Text("Latest Products",
